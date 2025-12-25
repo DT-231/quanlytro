094fd8b4d10115b8e34631672e25b6a97bcb0d1b
Nguyễn Văn Hoàng <97577134+DT-231@users.noreply.github.com>
2025-12-25 20:12:31 +0700

    Delete switch-acc directory

--- switch-acc/switch-main.sh
+++
@@ -1,4 +0,0 @@
-git remote set-url origin git@github.com:OWNER/REPO.git
-git config user.name "DT-231"
-git config user.email "hoang2312004@gmail.com"
-echo "✅ Switched to MAIN GitHub account"
--- switch-acc/switch-sub.sh
+++
@@ -1,4 +0,0 @@
-git remote set-url origin git@github-dt2312004:DT2312004/REPO.git
-git config user.name "DT2312004"
-git config user.email "nvnvh23@gmail.com"
-echo "✅ Switched to SUB GitHub account"
